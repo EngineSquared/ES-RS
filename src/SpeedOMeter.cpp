34e837e99a5617abb7674370cd4bd91c1791cd85
Divengerss <dorvann.ledeux@epitech.eu>
2025-09-06 11:28:38 +0200

    fix: getting the RPM and transmission mode from JoltPhysics

--- src/SpeedOMeter.cpp
+++ src/SpeedOMeter.cpp
@@ -21,24 +21,28 @@ void UpdateSpeedOmeter(ES::Engine::Core &core)
 
 void UpdateSpeedOmeterAnimations(ES::Engine::Core &core)
 {
-    float rpm = 0.0f;
-    const float minRPM = 0.0f;
-    const float maxRPM = 5000.0f;
+    float rpm = 0.0f;       // Default placeholder value, set by JoltPhysics
+    float minRPM = 0.0f;    // Default placeholder value, set by JoltPhysics
+    float maxRPM = 5000.0f; // Default placeholder value, set by JoltPhysics
     const float minAngle = 0.0f;
-    const float maxAngle = 230.0f;
+    const float maxAngle = 250.0f;
     int gear = 0;
+    JPH::ETransmissionMode transmissionMode = JPH::ETransmissionMode::Auto; // Default placeholder value, set by JoltPhysics
     const int ledCount = 41;
     const std::string &UITitle = core.GetResource<ES::Plugin::UI::Resource::UIResource>().GetTitle();
 
     if (UITitle == "game")
     {
         core.GetRegistry().view<ES::Plugin::Physics::Component::WheeledVehicle3D>().each(
-            [&core, &rpm, &gear](ES::Plugin::Physics::Component::WheeledVehicle3D &vehicle) {
+            [&core, &rpm, &gear, &maxRPM, &minRPM, &transmissionMode](ES::Plugin::Physics::Component::WheeledVehicle3D &vehicle) {
                 auto controller = reinterpret_cast<JPH::WheeledVehicleController *>(vehicle.vehicleConstraint->GetController());
                 if (controller)
                 {
                     rpm = controller->GetEngine().GetCurrentRPM();
                     gear = controller->GetTransmission().GetCurrentGear();
+                    minRPM = controller->GetEngine().mMinRPM;
+                    maxRPM = controller->GetEngine().mMaxRPM;
+                    transmissionMode = controller->GetTransmission().mMode;
                 }
             }
         );
@@ -50,11 +54,13 @@ void UpdateSpeedOmeterAnimations(ES::Engine::Core &core)
         });
         std::string formattedGear = std::to_string(gear);
 
-        if (gear == 0)
-            formattedGear = "N";
-        else if (gear == -1)
-            formattedGear = "R";
+        if (gear == 0) formattedGear = "N";
+        else if (gear == -1) formattedGear = "R";
         core.GetResource<ES::Plugin::UI::Resource::UIResource>().UpdateInnerContent("gear-current", formattedGear);
+        if (transmissionMode == JPH::ETransmissionMode::Auto)
+            core.GetResource<ES::Plugin::UI::Resource::UIResource>().UpdateInnerContent("transmission-mode", "AT");
+        else
+            core.GetResource<ES::Plugin::UI::Resource::UIResource>().UpdateInnerContent("transmission-mode", "MT");
 
         int ledsOn = static_cast<int>(std::round(t * ledCount));
         for (int i = 1; i <= ledCount; i++)
