e493c971965b1bf9bb1b2c94438e2b8294ff8124
Lucas Hauszler <lucas.hauszler@epitech.eu>
2026-01-24 21:24:36 +0100

    feat: new car obj

--- src/main.cpp
+++ src/main.cpp
@@ -37,7 +37,7 @@ void Setup(Engine::Core &core)
 {
     // Option to lock the cursor to the window
     auto &window = core.GetResource<Window::Resource::Window>();
-    window.MaskCursor();
+    // window.MaskCursor();
 
     CreateCheckeredFloor(core);
     auto vehicle = CreateVehicle(core);
@@ -74,7 +74,7 @@ class GraphicExampleError : public std::runtime_error {
 
 int main(void)
 {
-    spdlog::set_level(spdlog::level::err);
+    spdlog::set_level(spdlog::level::info);
     Engine::Core core;
 
     core.AddPlugins<Window::Plugin, DefaultPipeline::Plugin, Input::Plugin, CameraMovement::Plugin, Physics::Plugin>();
