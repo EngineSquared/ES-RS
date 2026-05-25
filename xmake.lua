5b8ab8440f07adcf529e64b5f63a052bb6c4ff47
Miouzora <91665686+Miou-zora@users.noreply.github.com>
2026-05-25 10:53:29 +0200

    feat: automatically pull e2

--- xmake.lua
+++ xmake.lua
@@ -1,24 +1,26 @@
 -- Build with xmake -P .
 set_project("ES-RS")
 set_languages("c++20")
+add_rules("mode.debug", "mode.release")
+
 
 if is_plat("windows") then
     add_cxflags("/W4")
 end
 
-includes("../EngineSquared/xmake.lua")
+add_repositories("EngineSquaredrepo https://github.com/EngineSquared/xrepo.git")
+
+add_requires("enginesquared ab1b71caa1110642a5e4ee3a56205ce428fb8cd0", { debug = is_mode("debug") })
 
 add_rules("plugin.compile_commands.autoupdate", {outputdir = ".vscode"})
 target("ES-RS")
     set_kind("binary")
 
-    add_deps("EngineSquared")
+    add_packages("enginesquared")
 
     add_files("src/**.cpp")
 
     add_includedirs("$(projectdir)/src/")
-
-    add_packages("entt", "glm", "glfw", "spdlog", "fmt", "joltphysics", "stb", "tinyobjloader", "wgpu-native", "glfw3webgpu", "lodepng", "rmlui", "miniaudio")
 
     set_rundir("$(projectdir)")
 
